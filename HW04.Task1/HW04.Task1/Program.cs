310a7e2555620630ec9f98264a99ae74949e1a28
Виктор <lucckyboy713@gmail.com>
2019-12-12 10:13:16 +0300

    Task 1 done

---
+++ HW04.Task1/HW04.Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW04.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int letters = 90; letters <= 90 && letters >= 65; letters--)
+            {
+                Console.WriteLine(Convert.ToChar(letters));
+            }
+        }
+    }
+}
