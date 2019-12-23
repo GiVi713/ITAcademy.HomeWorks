35b344a91638b22c937798c347cfdfe7d41fc13d
Виктор <lucckyboy713@gmail.com>
2019-12-23 11:51:30 +0300

    Second Task done

---
+++ Exercise2/Exercise2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int size = 5;
+            int[] nums = new int[size];
+            for (int i = 0; i < size - 1; i++)
+            {
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Введите последнее значение и его порядковый номер");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int pos = Convert.ToInt32(Console.ReadLine());
+            for (int i = size - 2; i >= pos - 1; i--)  
+                nums[i + 1] = nums[i];
+                nums[pos - 1] = num;
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+    }
+}
