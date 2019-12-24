02fcccf4d43e7f5b73f326037b21b1bab5375cbd
Виктор <lucckyboy713@gmail.com>
2019-12-24 20:55:04 +0300

    fix

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -5,30 +5,34 @@ namespace Task1
     class Program
     {
         static void Main(string[] args)
+        // {
+        //   int[] mas1 = new int[1];
+        //}
+        //static void Task2(string[] args)
         {
-            int[] mas = new int[1];
+            var mas2 = new object [3];
+            mas2[0] = 32;
+            mas2[1] = 'A';
+            mas2[2] = "Hello";
+            Console.WriteLine((int)mas2[0] + 10);
+            Console.WriteLine(mas2[2] + ", guys");
         }
-        static void Task2(string[] args)
+        /*static void Task3(string[] args)
         {
-            object[] mas = { 32, 'A', "Hello" };
-            Console.WriteLine(mas[0]);
-            Console.WriteLine(mas[2] + ", guys");
-        }
-        static void Task3(string[] args)
-        {
-            int[] mas = new int[13];
+            int[] mas3 = new int[13];
             Random rand = new Random();
             int max = 0;
             for (int i = 0; i < 13; i++)
             {
-                mas[i] = rand.Next();
-                Console.WriteLine(mas[i]);
-                if(mas[i] > max)
+                mas3[i] = rand.Next();
+                Console.WriteLine(mas3[i]);
+                if(mas3[i] > max)
                 {
-                    max = mas [i];
+                    max = mas3 [i];
                 } 
             }Console.WriteLine(max);
         }
+    }*/
     }
 }
 
