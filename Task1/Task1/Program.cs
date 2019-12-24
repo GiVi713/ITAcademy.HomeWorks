e3e1acd269afc5cebb4c344abe672b571bfc118f
Виктор <lucckyboy713@gmail.com>
2019-12-24 20:20:49 +0300

    task 3 fix

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -6,13 +6,30 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            object[] mas = new object[3];
-            mas[0] = "32";
-            mas[1] = "A";
-            mas[2] = "Hello";
+            int[] mas = new int[1];
+        }
+        static void Task2(string[] args)
+        {
+            object[] mas = { 32, 'A', "Hello" };
             Console.WriteLine(mas[0]);
             Console.WriteLine(mas[2] + ", guys");
-            }
+        }
+        static void Task3(string[] args)
+        {
+            int[] mas = new int[13];
+            Random rand = new Random();
+            int max = 0;
+            for (int i = 0; i < 13; i++)
+            {
+                mas[i] = rand.Next();
+                Console.WriteLine(mas[i]);
+                if(mas[i] > max)
+                {
+                    max = mas [i];
+                } 
+            }Console.WriteLine(max);
         }
     }
 }
+
+
