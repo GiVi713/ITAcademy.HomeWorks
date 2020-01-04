ccd4311c68c0e965bafe66ae32e1653d2d7e88a4
Виктор <lucckyboy713@gmail.com>
2020-01-04 15:03:11 +0300

    hw08 task 1 done

---
+++ HW08.Task1/HW08.Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace HW08.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sym = new StringBuilder("1a!2.3!!.. 4.!.?6 7! ..?");
+            Console.WriteLine(sym);
+            for (int i = 0; i < sym.Length ; i++)
+            {
+                if (sym[i] == '!')
+                {
+                    sym.Remove(i, 1);
+                    i--;
+                }           
+                if (sym[i] == '?')
+                {
+                    sym.Replace(' ', '_', i, sym.Length -i);
+                    break;
+                }
+            }  
+            Console.WriteLine(sym);
+        }
+    }
+}
