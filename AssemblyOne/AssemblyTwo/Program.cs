f1e2a4698415ebbc38783108b458b32996a55400
Виктор <lucckyboy713@gmail.com>
2020-01-05 20:14:48 +0300

    add something

--- AssemblyOne/AssemblyTwo/Program.cs
+++ AssemblyOne/AssemblyTwo/Program.cs
@@ -2,12 +2,22 @@
 using AssemblyOne;
 namespace AssemblyTwo
 {
-    class Program 
+    class Program
     {
         static void Main(string[] args)
         {
             Employee Test = new Employee();
             Test.name = "Yaropolk";
+        }
+        class Developer : Employee
+        {
+            public static void Dev()
+            {
+                Developer Test2 = new Developer();
+                Test2.name = "Yaroslav";
+                Test2.protect = -18;       
+                Test2.protintern = -16;
+            }
         }
     }
 }
