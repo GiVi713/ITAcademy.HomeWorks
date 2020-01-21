a83e6bc1ae65a6c020680d47a28e116264f6ac03
Виктор <lucckyboy713@gmail.com>
2020-01-22 01:47:32 +0300

    Second

--- HW_11.Task1/HW_11.Task1/Figures.cs
+++ HW_11.Task1/HW_11.Task1/Figures.cs
@@ -14,18 +14,47 @@ namespace HW_11.Task1
         public abstract double figiureArea();
     }
 
-    internal sealed class Square : Figures
+    class Square : Figures
     {
-        private double storona { get; set; }
-        public Square(string name, int storona) : base(name)
+        private double Storona { get; set; }
+        public Square(string name, double storona) : base(name)
         {
-
+            Storona = storona;
         }
 
-        public Square(string name) : base(name)
+        public override double figiureArea()
         {
+            return Math.Pow(storona,2);
         }
-
-        public override double figiureArea() => Math.Pow(2, 2);
     }
+    class Triangle : Figures
+    {
+        private double Sides { get; set; }
+        private double TriangleBase { get; set; }
+
+        public Triangle(string name, double sides, double triangleBase) : base(name) {
+            Sides = sides;
+            TriangleBase = triangleBase;
+        }
+
+        public override double figureArea()
+        {
+            double high = 0.5 * Math.Sqrt(4 * Math.Pow(Sides, 2) - Math.Pow(TriangleBase, 2));
+            return 0.5 * TriangleBase * high;
+        }
+    }
+    class Circle : Figures
+    {
+        private double Radius { get; set; }
+
+        public Circle(string name, double radius) : base (name)
+        {
+            Radius = radius;
+        }
+
+        public override double figiureArea()
+        {
+            return Math.Pow(Radius,2) * Math.PI;
+        }
+    } 
 }
--- HW_11.Task1/HW_11.Task1/Program.cs
+++ HW_11.Task1/HW_11.Task1/Program.cs
@@ -7,10 +7,18 @@ namespace HW_11.Task1
         static void Main(string[] args)
         {
             Random rand = new Random();
-            Array[] mas = new Array[3];
+            Figures[] mas = new Figures[3];
 
-            mas[0] = new Square("Sqaure", rand.Next(1, 10));
-             
+            mas[0] = new Square("Square", rand.Next(1, 10));
+            mas[1] = new Triangle("Square", rand.Next(1, 10),rand.Next(1,10));
+            mas[2] = new Circle("Circle", rand.Next(1, 10));
+        }
+        public void Result(Figures[] mas)
+        {
+            foreach (var item in mas)
+            {
+                Console.WriteLine($"This is {item.Name}.CLR Type is {item}.Square is {item.figiureArea()}");
+            }
         }
     }
 }
