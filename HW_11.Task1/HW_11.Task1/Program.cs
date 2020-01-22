3ba0762e55207ee29abf7a8eed1997cd5d752802
Виктор <lucckyboy713@gmail.com>
2020-01-22 12:12:41 +0300

    fs

---
+++ HW_11.Task1/HW_11.Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HW_11.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Figures[] mas = new Figures[3];
+
+            mas[0] = new Square("Square", rand.Next(1, 10));
+            mas[1] = new Triangle("Triangle", rand.Next(1, 10),rand.Next(1,10));
+            mas[2] = new Circle("Circle", rand.Next(1, 10));
+
+            Result(mas);
+        }
+
+        public static void Result(Figures[] mas)
+        {
+            foreach (var item in mas)
+            {
+                Console.WriteLine($"This is {item.Name}.CLR Type is {item.GetType()}.Square is {item.figiureArea()}");
+            }
+        }
+    }
+}
