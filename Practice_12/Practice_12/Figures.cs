ddc3e4c1737053f2e70b8a9ea3f24005eeaba309
Виктор <lucckyboy713@gmail.com>
2020-01-22 12:46:39 +0300

    Never too late

---
+++ Practice_12/Practice_12/Figures.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_12
+{
+    abstract class Figures
+    {
+        public string Name { get; set; }
+
+        public Figures ( string name)
+        {
+            Name = name;
+        }
+        public abstract double figureArea();
+    }
+
+    class Circle : Figures
+    {
+        private double _Radius { get; set; }
+
+        public Circle(string name, double _radius) : base(name)
+        {
+            _Radius = _radius;
+        }
+
+        public override double figureArea()
+        {
+            return Math.Round (2 * Math.PI * Math.Pow(_Radius, 2));
+        }
+    }
+
+    class Square : Figures
+    {
+        private double _SquareSide { get; set; }
+        
+        public Square (string name, double _squareSide) : base(name)
+        {
+            _SquareSide = _squareSide;
+        }
+
+        public override double figureArea()
+        {
+            return Math.Round (Math.Pow(_SquareSide, 2));
+        }
+    }
+
+    class Triangle : Figures
+    {
+        private double _TriangleSide { get; set; }
+
+        public Triangle(string name, double _triangleSide) : base(name)
+        {
+            _TriangleSide = _triangleSide;
+        }
+
+        public override double figureArea()
+        {
+            return Math.Round (0.5 * _TriangleSide * Math.Sqrt(Math.Pow(_TriangleSide, 2) - (Math.Pow(_TriangleSide, 2) / 4)));
+        }
+    }
+
+}
---
+++ Practice_12/Practice_12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Practice_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Figures[] mas = new Figures[5];
+
+            mas[0] = new Circle("Circle", rand.Next(1, 10));
+            mas[1] = new Square("Square", rand.Next(1, 10));
+            mas[2] = new Triangle("Triangle", rand.Next(1, 10));
+            mas[3] = new Circle("Circle", rand.Next(1, 10));
+            mas[4] = new Square("Square", rand.Next(1, 10));
+
+            Result(mas);
+        }
+
+        public static void Result ( Figures[] mas)
+        {
+            foreach ( var item in mas)
+            {
+                Console.WriteLine($"This is {item.Name}. CLR Type is {item}. Square is {item.figureArea()}");
+            }
+        }
+    }
+}
