eab1058cc3416a54e77a806ec8001fba435056e6
Виктор <lucckyboy713@gmail.com>
2020-01-28 19:55:23 +0300

    qq

--- Preoject 14.1/Project 14.3/Program.cs
+++ Preoject 14.1/Project 14.3/Program.cs
@@ -5,7 +5,7 @@ namespace Project_14._3
 {
     class Program
     {
-        public static List<Neighbor> Neighbor = new List<Neighbor> { };
+        public static List<Neighbor> floorNeighbors = new List<Neighbor> { };
 
         static void Main(string[] args)
         {
@@ -13,25 +13,22 @@ namespace Project_14._3
 
     class Neighbor
     {
-        Crud randomizer1 = new Crud();
+        Randomizer randomizer1 = new Randomizer();
         Random rand = new Random();
 
         public int ID { get; set; }
-        public string manufacturer { get; set; }
+        public string FullName { get; set; }
         public string state { get; set; }
         public string location { get; set; }
-        public int engineCapacity { get; set; }
-        public double price { get; set; }
+        public int PhoneNumber { get; set; }
+        public double flatNumber { get; set; }
 
         public Neighbor()
         {
-            
-            ID = randomizer1.GetID();
-            manufacturer = randomizer1.GetMark(rand.Next(1, 10));
-            state = randomizer1.GetState(rand.Next(1, 5));
+            FullName = randomizer1.GetName(rand.Next(1, 10));
             location = randomizer1.GetLocation(rand.Next(1, 10));
-            engineCapacity = rand.Next(400, 1200);               
-            price = rand.Next(1000, 20_000);                        
+            PhoneNumber = rand.Next(400, 1200);               
+            flatNumber = rand.Next(1, 30);                        
         }
     }
     }
---
+++ Preoject 14.1/Project 14.3/Randomizer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_14._3
+{
+    class Randomizer
+    {
+        Random rand = new Random();
+        static int Id = 0;
+
+        public int GetID()
+        {
+            
+            Id++;
+            return Id;
+        }
+
+        public string GetName(int num)
+        {
+            
+            string FullName = "";
+            switch (num)
+            {
+                case 1: FullName = "BMV"; break;
+                case 2: FullName = "Ducati"; break;
+                case 3: FullName = "Honda"; break;
+                case 4: FullName = "Kawasaki"; break;
+                case 5: FullName = "Yamaha"; break;
+                case 6: FullName = "Suzuki"; break;
+                case 7: FullName = "HarleyDavidson"; break;
+                case 8: FullName = "Aprilia"; break;
+                case 9: FullName = "KTM"; break;
+                case 10: FullName = "Triumph"; break;
+            }
+            return FullName;
+        }
+
+        public string GetState(int num)
+        {
+           
+            string state = "";
+            switch (num)
+            {
+                case 1: state = "Новый"; break;
+                case 2: state = "Б/У"; break;
+                case 3: state = "Старый"; break;
+                case 4: state = "На запчасти"; break;
+                case 5: state = "Хорошее"; break;
+            }
+            return state;
+        }
+
+        public string GetLocation(int num)
+        {
+            Logger.Log.Debug("Get moto location");
+            string location = "";
+            switch (num)
+            {
+                case 1: location = "Минск"; break;
+                case 2: location = "Брест"; break;
+                case 3: location = "Витебск"; break;
+                case 4: location = "Гродно"; break;
+                case 5: location = "Гомель"; break;
+                case 6: location = "Могилев"; break;
+                case 7: location = "Логойск"; break;
+                case 8: location = "Лида"; break;
+                case 9: location = "Жлобин"; break;
+                case 10: location = "Жодино"; break;
+            }
+            return location;
+        }
+
+    }
+}
