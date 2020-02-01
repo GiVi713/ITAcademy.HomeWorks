ec6a38b2843f1784783e914be607816a8853a98c
Виктор <lucckyboy713@gmail.com>
2020-02-01 18:10:25 +0300

    First Com

---
+++ FinalProject/FinalProject/IUserData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject
+{
+    interface IUserData
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string UserLocation { get; set; }
+        public double UserAverageMark { get; set; }
+        public string AccessLevel{ get; set; }
+    }
+}
