453995b73106926bd66b4c682297b2c38d1a17ba
Виктор <lucckyboy713@gmail.com>
2020-02-02 22:52:02 +0300

    AnotherOne

--- FinalProject/BlackMirror/View/HomeView.xaml.cs
+++ FinalProject/BlackMirror/View/HomeView.xaml.cs
@@ -1,9 +1,5 @@
-﻿
-using BlackMirror.View;
-using BlackMirror.ViewModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,17 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BlackMirror
+namespace BlackMirror.View
 {
-    public partial class MainWindow : Window
+    /// <summary>
+    /// Interaction logic for HomeView.xaml
+    /// </summary>
+    public partial class HomeView : Window
     {
-        public MainWindow()
+        public HomeView()
         {
             InitializeComponent();
-            DataContext = new MainViewModel();
         }
     }
 }
